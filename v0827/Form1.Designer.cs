86c7616e9a458c38d7f50f8733e9693d88ffa3bb
Citrus.T <datdaisukemikancha@gmail.com>
2020-08-27 11:13:07 +0900

    演習11-1

--- v0827/Form1.Designer.cs
+++ v0827/Form1.Designer.cs
@@ -37,11 +37,13 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.label1.Location = new System.Drawing.Point(12, 55);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.Size = new System.Drawing.Size(93, 21);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "(*‘ω‘ *)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
--- v0827/Form1.cs
+++ v0827/Form1.cs
@@ -11,8 +11,10 @@ using System.Windows.Forms;
 namespace v0827
 {
     public partial class Form1 : Form
-        int vx,vy;
     {
+        int vx = -10;
+        int vy = -10;
+
         public Form1()
         {
             InitializeComponent();
@@ -23,14 +25,44 @@ namespace v0827
             label1.Left += vx;
             label1.Top += vy;
 
-            label1.Left = -10;
-            label1.Top = -10;
-
+            if(label1.Left < 0)
+            {
+                vx = Math.Abs(vx);
+            }
+            if(label1.Top < 0)
+            {
+                vy = Math.Abs(vy);
+            }
+            if(label1.Right > ClientSize.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+            if(label1.Bottom > ClientSize.Height)
+            {
+                vy = -Math.Abs(vy);
+            }
+            // 2次元クラスPoint型の変数mpを宣言
             Point mp = MousePosition;
 
-            mp = Point to Client(mp);
+            // mpに、マウスのフォーム座標を取り出す。
+            //// MousePositionにマウス座標のスクリーン左上からのX、Yが入っている。
+            //// PointToClient()を使うと、スクリーン座標を、フォーム座標に変換できる。
+            mp = PointToClient(mp);
 
+            // ラベルに座標を表示
+            //// 変換したフォーム座標は、mp.X、mp.Yで取り出せる。
+            label2.Text = "" + mp.X + "," + mp.Y;
 
+            // aが0で、かつ、bが0
+            if ((mp.X >= label1.Left) && (mp.X < label1.Right) && (mp.Y >= label1.Top) && (mp.Y < label1.Bottom))
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
         }
     }
 }
